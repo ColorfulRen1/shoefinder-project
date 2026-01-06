3dbb0266804ad6760d9313b9db9eca0a3f98d7a2
ColorfulRen1 <115109874+ColorfulRen1@users.noreply.github.com>
2026-01-06 14:32:42 -0800

    Update background.js

--- background/background.js
+++ background/background.js
@@ -1,1 +1,13 @@
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.type === "GET_SIZE") {
+    const rules = {
+      Nike: 0,
+      Adidas: 0.5,
+      "New Balance": -0.5
+    };
+
+    const adjustment = rules[msg.brand] ?? 0;
+    sendResponse({ suggestedSize: msg.size + adjustment });
+  }
+});
 
